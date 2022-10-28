6c0f594250215f25499d274f0ff8683d0d9552bd
Walter Cun Bustamante <waltercunbustamante@gmail.com>
2022-10-27 19:07:27 -0500

    Agregar descripcion de documentacion

--- src/modules/admin-org/dto/org.dto.ts
+++ src/modules/admin-org/dto/org.dto.ts
@@ -2,12 +2,19 @@ import { ApiProduces, ApiProperty, PartialType } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsPositive, IsString } from "class-validator";
 
 export class CreateAdminOrgDto {
-    @ApiProperty()
+    @ApiProperty({
+        description: 'Identificacion de la Organizacion',
+        required: true,
+      })
     @IsString()
     @IsNotEmpty()
     readonly name: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        description: 'Estatus de la Organizacion',
+        minimum: 0,
+        required: true,
+      })
     @IsNumber()
     @IsNotEmpty()
     @IsPositive()
